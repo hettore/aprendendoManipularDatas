ea0de8218fe4ff08c56cc4458f9d4e9b2881b58b
Hettore Eduardo <duh.barretoo@gmail.com>
2022-08-24 16:40:38 -0300

    Convertendo hora.

--- src/application/Program.java
+++ src/application/Program.java
@@ -81,10 +81,12 @@ public class Program {
 		System.out.println("r3 = " + r3);
 		System.out.println("r4 = " + r4);
 		
+		System.out.println("d04 dia = " + d04.getDayOfMonth());
+		System.out.println("d04 mês = " + d04.getMonthValue());
+		System.out.println("d04 ano = " + d04.getYear());
 		
-		
-		
-		
+		System.out.println("d05 hora = " + d05.getHour());
+		System.out.println("d05 hora = " + d05.getMinute());
 		
 	}
 

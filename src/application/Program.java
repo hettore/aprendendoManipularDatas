8e9dae673300a539591fab959c74b769e88e0e5e
Hettore Eduardo <duh.barretoo@gmail.com>
2022-08-24 16:59:28 -0300

    Cálculos com data-hora.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Program {
 
@@ -67,26 +67,59 @@ public class Program {
 //		System.out.println("d06 = " + d06.toString());
 		
 		
-		LocalDate d04 = LocalDate.parse("2022-02-25");
-		LocalDateTime d05 = LocalDateTime.parse("2022-02-25T01:30:26");
+//		LocalDate d04 = LocalDate.parse("2022-02-25");
+//		LocalDateTime d05 = LocalDateTime.parse("2022-02-25T01:30:26");
+//		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+//		
+//		LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+//		LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+//		LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+//		LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+//		
+//		System.out.println("r1 = " + r1);
+//		System.out.println("r2 = " + r2);
+//		System.out.println("r3 = " + r3);
+//		System.out.println("r4 = " + r4);
+//		
+//		System.out.println("d04 dia = " + d04.getDayOfMonth());
+//		System.out.println("d04 mês = " + d04.getMonthValue());
+//		System.out.println("d04 ano = " + d04.getYear());
+//		
+//		System.out.println("d05 hora = " + d05.getHour());
+//		System.out.println("d05 hora = " + d05.getMinute());
+		
+		LocalDate d04 = LocalDate.parse("2022-07-20");
+		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
 		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 		
-		LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
-		LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
-		LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
-		LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+		LocalDate pastWeekLocalDate = d04.minusDays(7);
+		LocalDate nextWeekLocalDate = d04.plusDays(7);
 		
-		System.out.println("r1 = " + r1);
-		System.out.println("r2 = " + r2);
-		System.out.println("r3 = " + r3);
-		System.out.println("r4 = " + r4);
+		System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+		System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
 		
-		System.out.println("d04 dia = " + d04.getDayOfMonth());
-		System.out.println("d04 mês = " + d04.getMonthValue());
-		System.out.println("d04 ano = " + d04.getYear());
+		LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+		LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
 		
-		System.out.println("d05 hora = " + d05.getHour());
-		System.out.println("d05 hora = " + d05.getMinute());
+		System.out.println("pastWeekLocalDateTime = " + pastWeekLocalDateTime);
+		System.out.println("nextWeekLocalDateTime = " + nextWeekLocalDateTime);
+		
+		Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+		Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+		
+		System.out.println("pastweekInstant = " + pastWeekInstant);
+		System.out.println("nextweekInstant = " + nextWeekInstant);
+		
+		Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay());
+		Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+		Duration t3 = Duration.between(pastWeekInstant, d06);
+		Duration t4 = Duration.between(d06, pastWeekInstant);
+		
+		System.out.println("t2 dias = " + t2.toDays());
+		System.out.println("t1 dias = " + t1.toDays());
+		System.out.println("t3 dias = " + t3.toDays());
+		System.out.println("t4 dias = " + t4.toDays());
+		
 		
 	}
 
